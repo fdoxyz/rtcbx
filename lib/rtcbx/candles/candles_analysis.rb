a0d0919803766bf03eb3cdaf0798bc23d61e440c
Fernando Valverde Arredondo <fdov88@gmail.com>
2017-12-11 10:47:36 +0100

    candles#average_movement

--- lib/rtcbx/candles/candles_analysis.rb
+++ lib/rtcbx/candles/candles_analysis.rb
@@ -4,13 +4,18 @@ class RTCBX
     # methods for calculating whatever it is you feel like calculating.
     #
     module CandlesAnalysis
+      # Number of all current candles tracked
       def candles_count
         @candles.count
       end
 
-      def average_price_movement(last_n = 10)
-        movements = @candles.last(last_n).map { |candle| candle.high - candle.low }
-        movements.sum / movements.count
+      # The average movement (from high to low) from the last_n candles.
+      # moving_candles param will filter out candles without movement
+      def average_movement(last_n: 10, non_zero: false)
+        target_candles = @candles unless non_zero
+        target_candles ||= @candles.select { |c| (c.high - c.low) > 0 }
+        movements = target_candles.last(last_n).map { |c| c.high - c.low }
+        movements.sum / BigDecimal.new(movements.count)
       end
     end
   end
